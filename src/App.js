608afdee3716b5aa74c1233912833521873f2654
Muskan Jaiswal <muskanjaiswal710@gmail.com>
2020-11-04 18:36:21 +0530

    banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>   
           <Route path= "/login">
@@ -16,6 +18,7 @@ function App() {
           {/*This is default route*/}
           <Route path="/">
             <Header />
+            <Home />
           <h1>Home PAGE!!</h1>
           </Route>
         </Switch>
