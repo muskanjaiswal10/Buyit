e4d88c3fd7befbac561536385e2914bde3296f42
Muskan Jaiswal <muskanjaiswal710@gmail.com>
2020-11-07 17:39:24 +0530

    Subtotal Component

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {useStateValue} from "./StateProvider";
 import './Checkout.css';
 import CheckoutProduct from './CheckoutProduct';
+import './Checkout.css';
+import Subtotal from "./Subtotal";
 
 function Checkout() {
     const [{basket}] = useStateValue();
@@ -15,6 +17,7 @@ function Checkout() {
              {basket?.length === 0 ? (
              <div>
                 <h2> Your Shopping Basket is Empty.</h2>
+                <p>You have no items in your basket. To buy one click on "Add to basket" next to the item.</p>
              </div>) : (
              <div>
                 <h2 className="checkout_title">Your Shopping Basket.</h2> 
@@ -33,6 +36,10 @@ function Checkout() {
 
                 )}
             </div>
+            <div className = "checkout_right">
+             <Subtotal />
+            </div>
+              
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,9 @@ export const initialState ={
     }],
 
 };
+
+export const getBasketTotal = (basket) =>
+basket?.reduce((amount,item) => item.price+amount, 0);
 const reducer = (state, action) => {
     console.log(action);
     switch(action.type){
